d4abb41059b1c8df9cbde6ae0f02819b46d20d90
Alberto Bautista <dachb101@gmail.com>
2025-10-25 11:51:14 -0600

    Remove how we work menu option

--- src/app/components/Menu/contants.ts
+++ src/app/components/Menu/contants.ts
@@ -1,9 +1,8 @@
 // constants.ts
-export type MenuKey = "home" | "how-we-work" | "our-story" | "reviews" | "faqs";
+export type MenuKey = "home" | "our-story" | "reviews" | "faqs";
 
 export const menuItems: Array<{ id: MenuKey; href: string }> = [
   { id: "home", href: "#" },
-  { id: "how-we-work", href: "#" },
   { id: "our-story", href: "#" },
   { id: "reviews", href: "#" },
   { id: "faqs", href: "#" },
